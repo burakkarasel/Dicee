73f936e69d6433e084b2a247932bedb0ffeb6f4f
Burak Can Karasel <99825584+burakkarasel@users.noreply.github.com>
2023-08-04 15:40:35 +0300

    First version of Dicee completed

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDice: UIImageView!
+    @IBOutlet weak var secondDice: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func roll(_ sender: Any) {
+        let images = [
+            UIImage(named: "DiceOne"),
+            UIImage(named: "DiceTwo"),
+            UIImage(named: "DiceThree"),
+            UIImage(named: "DiceFour"),
+            UIImage(named: "DiceFive"),
+            UIImage(named: "DiceSix"),
+        ]
+        
+        firstDice.image = images.randomElement()!
+        secondDice.image = images.randomElement()!
+    }
+    
 }
 
